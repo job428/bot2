546125063f8624fd7b8b282c176baff95606b6ac
job428 <chaimala335@gmail.com>
2021-05-27 22:18:41 +0700

    git commit

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const PORT = process.env.PORT || 9999;
 const request = require('request');
 const bodyParser = require('body-parser');
 
+
 server()
     .use(bodyParser.json())
     .use(bodyParser.urlencoded({ extended: false}))
     .get('/', (req, res) => res.send(`Hi there! This is a nodejs-line-api running on PORT: ${ PORT }`))
-    .listen(PORT, () => console.log(`Listening on ${ PORT }`));P
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`));
